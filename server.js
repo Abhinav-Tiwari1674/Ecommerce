4d1c985a551c78d3e1718368c80d1f79f3b3abfb
Abhinav-Tiwari1674 <abhinav.tiwari_cs23@gla.ac.in>
2025-08-04 23:47:00 +0530

    ✅ MongoDB connection fixed 2

--- server.js
+++ server.js
@@ -3,19 +3,23 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const productRoutes = require('./routes/productRoutes');
 
-const app = express(); // 🔴 ye sabse zaroori line thi!
+const app = express(); // ✅ Server initialized
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://abhinavtiwarics23:MEiUkhEbFJauqmuk@cluster0.9xvolgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://abhinavtiwarics23:eT8sULhnYVf6NBej@Cluster0.9xvolgv.mongodb.net/ecommerceDB?retryWrites=true&w=majority')
   .then(() => console.log("✅ MongoDB Connected"))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log("❌ MongoDB Connection Error:", err));
 
+
+
+// Routes
 app.use('/', productRoutes);
 
+// Port setup
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(` Server running at http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
