74f5a1365719007c64c202afcd0171dc3dc4c662
Abhinav-Tiwari1674 <abhinav.tiwari_cs23@gla.ac.in>
2025-08-04 22:41:02 +0530

    Fix: Correct MongoDB URI and PORT for Render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/ecommerceDB')
+mongoose.connect('mongodb+srv://abhinavtiwarics23:MEiUkhEbFJauqmuk@cluster0.9xvolgv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log(err));
 
@@ -17,5 +17,5 @@ app.use('/', productRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log(` Server running at http://localhost:${PORT}`);
 });
